b8b6f8553b9d66ca748253bd9cf55e0d41c6bd2c
Aaron Freidus <aaronf101@gmail.com>
2016-06-09 10:13:12 -0700

    Fixes white space issue on right side of small screens on slides view when menu is toggled

--- client/slides.jsx
+++ client/slides.jsx
@@ -5,35 +5,35 @@ import Slider from 'react-viewport-slider';
 const Slides = () => (
   <Slider>
     <div itemStyle={{ backgroundColor: '#002D8E' }}>
-      <div className="Aligner container centering text-center">
+      <div className="Aligner  centering text-center">
         <div className="col-md-8 Aligner-item choiceContainer">
           <div className="content h1">You</div>
         </div>
       </div>
     </div>
     <div itemStyle={{ backgroundColor: '#00B1F2' }}>
-      <div className="Aligner container centering text-center">
+      <div className="Aligner centering text-center">
         <div className="col-md-8 Aligner-item choiceContainer">
           <div className="content h1">Casual</div>
         </div>
       </div>
     </div>
-    <div itemStyle={{ backgroundColor: '#0099F2' }}>
-      <div className="Aligner container centering text-center">
+    <div itemStyle={{ backgroundColor: '#0099F2'}}>
+      <div className="Aligner  centering text-center">
         <div className="col-md-8 Aligner-item choiceContainer">
           <div className="content h1">Professional</div>
         </div>
       </div>
     </div>
     <div itemStyle={{ backgroundColor: '#F2D500' }}>
-      <div className="Aligner container centering text-center">
+      <div className="Aligner  centering text-center">
         <div className="col-md-8 Aligner-item choiceContainer">
           <div className="content h1">Outdoors</div>
         </div>
       </div>
     </div>
     <div itemStyle={{ backgroundColor: '#C9009D' }}>
-      <div className="Aligner container centering text-center">
+      <div className="Aligner  centering text-center">
         <div className="col-md-8 Aligner-item choiceContainer">
           <div className="content h1">You're done!</div>
         </div>
