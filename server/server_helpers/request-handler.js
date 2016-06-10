85d142807145a703b49d40380bf03f796cb9f61f
Felix <felix2feng@gmail.com>
2016-06-10 10:37:48 -0700

    Rebasing

--- server/server_helpers/request-handler.js
+++ server/server_helpers/request-handler.js
@@ -8,16 +8,16 @@ const url = require('url');
 const Users = require('../db/collections/users');
 const User = require('../db/models/user');
 
-const Arc = require('../db/models/arc');
-const Arcs = require('../db/collections/arcs');
+// const Arc = require('../db/models/arc');
+// const Arcs = require('../db/collections/arcs');
 
-const Image = require('../db/models/image.js');
-const Images = require('../db/collections/images.js');
+// const Image = require('../db/models/image.js');
+// const Images = require('../db/collections/images.js');
 
 const helpers = require('./helpers.js');
 const categories = require('./categories.js');
 
-const limit = 5;
+// const limit = 5;
 
 module.exports.main = {
   get(req, res) {
@@ -59,87 +59,87 @@ module.exports.signin = {
 };
 
 // CREATE: Store obj from API calls to DB
-module.exports.create = {
-  get(req, res) {
-    res.send('success');
-  },
+// module.exports.create = {
+//   get(req, res) {
+//     res.send('success');
+//   },
 
-  post(req, res) {
-    // store obj from fb api calls into db
-    console.log('post request from client', req.body.photos.data.length);
-    const imgUrl = helpers.minimizeAndRandArr(req.body.photos.data, limit);
-      // user has already been created
-    User.forge({ fbId: req.body.id })
-      .fetch()
-      .then((userMatched) => {
-        // make new arc
-        const arc = new Arc({
-          name: Date(),
-        });
-        return arc.save({ user_id: userMatched.id });
-      })
-      .then((newArc) => {
-        console.log('Images in arc =>', imgUrl);
+//   post(req, res) {
+//     // store obj from fb api calls into db
+//     console.log('post request from client', req.body.photos.data.length);
+//     const imgUrl = helpers.minimizeAndRandArr(req.body.photos.data, limit);
+//       // user has already been created
+//     User.forge({ fbId: req.body.id })
+//       .fetch()
+//       .then((userMatched) => {
+//         // make new arc
+//         const arc = new Arc({
+//           name: Date(),
+//         });
+//         return arc.save({ user_id: userMatched.id });
+//       })
+//       .then((newArc) => {
+//         console.log('Images in arc =>', imgUrl);
 
-      // store img into new arc
-        for (let imgId = 0; imgId < imgUrl.length; imgId++) {
-          const imgSizeArr = imgUrl[imgId].images;
-          // for (var imgSize = 0; imgSize < imgSizeArr.length; imgSize++) {
-            // var img = imgSizeArr[imgSize];
-          const img = imgSizeArr[0];
-          console.log('Img instance', img);
-          const image = new Image({
-            height: img.height,
-            width: img.width,
-            url: img.source,
-          });
+//       // store img into new arc
+//         for (let imgId = 0; imgId < imgUrl.length; imgId++) {
+//           const imgSizeArr = imgUrl[imgId].images;
+//           // for (var imgSize = 0; imgSize < imgSizeArr.length; imgSize++) {
+//             // var img = imgSizeArr[imgSize];
+//           const img = imgSizeArr[0];
+//           console.log('Img instance', img);
+//           const image = new Image({
+//             height: img.height,
+//             width: img.width,
+//             url: img.source,
+//           });
 
-          image.save({ arc_id: newArc.id });
-            // console.log("A new img has been added => ", image);
-          // }
-        }
-      });
-    res.send('success');
-  },
-};
+//           image.save({ arc_id: newArc.id });
+//             // console.log("A new img has been added => ", image);
+//           // }
+//         }
+//       });
+//     res.send('success');
+//   },
+// };
 
 //
-module.exports.dashboard = {
-  get(req, res) {
-    const urlParts = url.parse(req.url, true); // Parse URL
-    const userId = urlParts.query.user_id; // Get user ID from URL
-    const results = [];
-    User.forge({ fbId: userId }) // Create new user with ID
-      .fetch()
-      .then((userMatched) => {
-        Arcs.reset() // Empty out the current collection of story Arcs
-          .query({ where: { user_id: userMatched.id } }) // Query userID's arcs
-          .fetch()
-          .then((arcMatched) => {
-            // make array of matching arc id using recursive function next
-            const next = (index) => {
-              if (index === arcMatched.length) {
-                res.json(results);
-                return;
-              }
-              Images.reset()
-                .query((qb) => {
-                  qb.where('arc_id', '=', arcMatched.models[index].id);
-                })
-                .fetch()
-                .then((imageMatched) => {
-                  console.log('for index...', index);
-                  // loop through all images in each arc and return an array
-                  const imageArray = helpers.createArrayOfPhotos(imageMatched);
-                  results.push(imageArray);
-                  next(index + 1);
-                });
-            };
-            next(0);
-          });
-      });
-  },
-};
+// module.exports.dashboard = {
+//   get(req, res) {
+//     const urlParts = url.parse(req.url, true); // Parse URL
+//     const userId = urlParts.query.user_id; // Get user ID from URL
+//     const results = [];
+//     User.forge({ fbId: userId }) // Create new user with ID
+//       .fetch()
+//       .then((userMatched) => {
+//         Arcs.reset() // Empty out the current collection of story Arcs
+//           .query({ where: { user_id: userMatched.id } }) // Query userID's arcs
+//           .fetch()
+//           .then((arcMatched) => {
+//             // make array of matching arc id using recursive function next
+//             const next = (index) => {
+//               if (index === arcMatched.length) {
+//                 res.json(results);
+//                 return;
+//               }
+//               Images.reset()
+//                 .query((qb) => {
+//                   qb.where('arc_id', '=', arcMatched.models[index].id);
+//                 })
+//                 .fetch()
+//                 .then((imageMatched) => {
+//                   console.log('for index...', index);
+//                   // loop through all images in each arc and return an array
+//                   const imageArray = helpers.createArrayOfPhotos(imageMatched);
+//                   results.push(imageArray);
+//                   next(index + 1);
+//                 });
+//             };
+//             next(0);
+//           });
+//       });
+//   },
+// };
 
 module.exports.categorize = {
   post(req, res) {
